bc93c9df42bad5eba0adee5106849f12620c9e7d
Nitak22 <nitakhadke22@gmail.com>
2019-04-05 15:50:10 -0400

    initial commit fixes AB#1

---
+++ src/webparts/spgithubwebinarWebpart/components/SpgithubwebinarWebpart.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import styles from './SpgithubwebinarWebpart.module.scss';
+import { ISpgithubwebinarWebpartProps } from './ISpgithubwebinarWebpartProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export default class SpgithubwebinarWebpart extends React.Component<ISpgithubwebinarWebpartProps, {}> {
+  public render(): React.ReactElement<ISpgithubwebinarWebpartProps> {
+    return (
+      <div className={ styles.spgithubwebinarWebpart }>
+        <div className={ styles.container }>
+          <div className={ styles.row }>
+            <div className={ styles.column }>
+              <span className={ styles.title }>Welcome to SharePoint!</span>
+              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
+              <p className={ styles.description }>{escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" className={ styles.button }>
+                <span className={ styles.label }>Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/webparts/spgithubwebinarWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpgithubwebinarWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpgithubwebinarWebpartWebPartStrings' {
+  const strings: ISpgithubwebinarWebpartWebPartStrings;
+  export = strings;
+}
